e50e1b5be70a03c87ecc536d1d6da0c8f2596686
CamiAnabel34 <annasa348@gmail.com>
2025-11-09 20:38:12 -0300

    sincronizo la version limpia

--- src/main/java/com/mycompany/discordbot2/DiscordBot.java
+++ src/main/java/com/mycompany/discordbot2/DiscordBot.java
@@ -7,7 +7,7 @@ import java.io.OutputStream;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-public class DiscordBot2 {
+public class DiscordBot {
     public static void main(String[] args) {
 
         String TOKEN_SECRETO = System.getenv("DISCORD_TOKEN");
